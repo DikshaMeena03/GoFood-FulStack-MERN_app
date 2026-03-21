eb251f51309989393fb7dcb803253591054eb048
Diksha <dikshameena2003@gmail.com>
2026-03-21 23:18:26 +0530

    database setup

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,9 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb://localhost:27017/GetFoodMERN';
-
-
 
 const mongoDB = async () => {
   try {
-    await mongoose.connect(mongoURI);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected Successfully");
-
-   
   } catch (error) {
     console.error("Connection error:", error);
     process.exit(1);
--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express()
 const port = 5000
+require('dotenv').config();
 const mongoDB = require("./db");
 mongoDB();
 
