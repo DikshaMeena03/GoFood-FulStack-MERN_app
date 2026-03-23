e2713c2db719ca7bd7528a7531b5325ec78072cd
Diksha <dikshameena2003@gmail.com>
2026-03-23 22:09:00 +0530

    corrected

--- backend/Routes/OrderData.js
+++ backend/Routes/OrderData.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Order = require('../models/Order')
 
-router.post('/orderData', async (req, res) => {
+router.post("/orderData", async (req, res) => {
     let data = req.body.order_data
     await data.splice(0,0,{Order_date:req.body.order_date})
     console.log("1231242343242354",req.body.email)
