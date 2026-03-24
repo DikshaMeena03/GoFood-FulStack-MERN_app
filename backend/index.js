c6c7b65a1b9f1e4a007eace3562b2eb1ccf697af
Diksha <dikshameena2003@gmail.com>
2026-03-24 20:10:27 +0530

    fix API URL for production

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const mongoDB = require("./db");
 mongoDB();
 
 app.use((req, res, next) =>{
-  res.setHeader("Access-Control-Allow-Origin","https://go-food-ful-stack-mern-app.vercel.app/");
+  res.setHeader("Access-Control-Allow-Origin","http://localhost:3000","https://go-food-ful-stack-mern-app.vercel.app/");
   res.header("Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
