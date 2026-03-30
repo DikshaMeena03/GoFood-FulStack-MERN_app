e20d84668fce35124ff48088f8c28e75dc168d11
Diksha <dikshameena2003@gmail.com>
2026-03-30 17:14:30 +0530

    fix: image links

--- src/screens/AboutUs.js
+++ src/screens/AboutUs.js
@@ -37,7 +37,7 @@ const AboutUs = () => {
         <div className="container">
           <div className="row align-items-center">
             <div className="col-md-6">
-              <img src="/about-vegetable.jpg" className="img-fluid rounded" alt="" style={{
+              <img src="/about-vegetables.jpg" className="img-fluid rounded" alt="" style={{
                   width: "100%",
                   height: "400px",
                   objectFit: "cover",
