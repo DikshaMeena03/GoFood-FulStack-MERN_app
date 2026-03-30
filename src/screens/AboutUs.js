67840f56768edfd477d605d8b79a798c053a9451
Diksha <dikshameena2003@gmail.com>
2026-03-30 17:05:42 +0530

    updated image

--- src/screens/AboutUs.js
+++ src/screens/AboutUs.js
@@ -37,8 +37,7 @@ const AboutUs = () => {
         <div className="container">
           <div className="row align-items-center">
             <div className="col-md-6">
-              <img
-                src="/about-vegetable.jpg" className="img-fluid rounded" alt="" style={{
+              <img src="/about-vegetable.jpg" className="img-fluid rounded" alt="" style={{
                   width: "100%",
                   height: "350px",
                   objectFit: "cover",
