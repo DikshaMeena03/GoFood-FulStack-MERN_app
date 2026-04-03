3e7852c9f86d285d183a8aabf3776019663c87b2
Diksha <dikshameena2003@gmail.com>
2026-04-04 00:26:47 +0530

    fix: credential popups

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -24,7 +24,7 @@ router.post("/createuser",
                 name: req.body.name,
                 email: req.body.email,
                 password: secPassword,
-                geolocation: req.body.geolocation
+                location: req.body.geolocation 
             })
             res.json({ success: true });
         }
